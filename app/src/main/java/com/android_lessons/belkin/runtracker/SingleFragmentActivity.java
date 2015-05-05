34b4edf0e5d5b85535348eea5f39c322d0168a19
Belka <Evgenia_@email.ua>
2015-05-05 12:56:52 +0300

    add single fragment

---
+++ app/src/main/java/com/android_lessons/belkin/runtracker/SingleFragmentActivity.java
@@ -0,0 +1,35 @@
+package com.android_lessons.belkin.runtracker;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by Belkin on 02.04.2015.
+ */
+public abstract class SingleFragmentActivity extends ActionBarActivity { //FragmentActivity {
+
+    protected abstract Fragment createFragment();
+
+    protected int getLayoutResId() {
+        return R.layout.activity_fragment;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResId());
+
+        FragmentManager fm = getSupportFragmentManager(); //getFragmentManager(); без поддержки совместимости
+
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+        if (fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragmentContainer, fragment)
+                    .commit();
+        }
+
+    }
+}
